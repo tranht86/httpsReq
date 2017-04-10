452dd1fed3d9b41ae50f494025f3a9f1e6b26faf
tranht86 <tran_ht@hotmail.com>
2017-04-10 16:31:28 +0000

    Step 1 GET data print chunks

--- main.js
+++ main.js
@@ -5,5 +5,26 @@ function getAndPrintHTMLChunks() {
     path: '/http-exmaples/step2.html'
   };
 
+  var https = require('https');
 
+  https.get(requestOptions, function (response) {
+
+    //set encoding to UTF-8
+    response.setEncoding('utf8');
+
+    //the callback is invoked when 'data' chunk is received
+    response.on('data', function (data) {
+      console.log('Chunk received. Length: ', data.length);
+
+    });
+
+    // the callback is invoked when all of the data has been received
+    // the 'end' of the stream
+    response.on('end', function () {
+      console.log('Response stream complete.');
+    });
+  })
 }
+
+
+getAndPrintHTMLChunks()
