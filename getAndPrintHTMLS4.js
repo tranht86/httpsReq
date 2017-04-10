7dd17bccf4c20aa362968709e70dd6f207a0659d
tranht86 <tran_ht@hotmail.com>
2017-04-10 18:19:33 +0000

    Callback function implemented

--- getAndPrintHTMLS4.js
+++ getAndPrintHTMLS4.js
@@ -1,28 +1,33 @@
-function getAndPrintHTML (options) {
+function getHTML (options, callback) {
 
+  /* Add your code here */
   var https = require('https')
   var a = ""
 
   https.get(options, function(response) {
-
-    response.setEncoding('utf8');
+    response.setEncoding('utf8')
 
     response.on('data', function(data) {
-      a += data;
+      a += data
       console.log('Chunk received. Length: ', data.length);
     });
 
-    response.on('end', function(){
-      console.log('Full stream data at a: ', a);
-      console.log('Response stream complete.')
-    })
+    response.on('end', function() {
+    console.log('Full stream data at a: ', a);
+    console.log('Response stream complete');
 
-  })
+    });
+
+  });
+}
+
+function printHTML (html) {
+  console.log(html);
 }
 
 var requestOptions = {
   host: 'sytantris.github.io',
-  path: '/http-examples/step3.html'
+  path: '/http-examples/step4.html'
 };
 
-getAndPrintHTML(requestOptions);
+getHTML(requestOptions, printHTML)
