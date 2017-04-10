e9eb33ffcb5ff212f7096a066f79e8e801733888
tranht86 <tran_ht@hotmail.com>
2017-04-10 18:40:54 +0000

    Module has been made

---
+++ http-functions.js
@@ -0,0 +1,34 @@
+module.exports = function getHTML (options, callback) {
+
+  /* Add your code here */
+  var https = require('https')
+
+  var a = ""
+
+  https.get(options, function(response) {
+    response.setEncoding('utf8')
+
+    response.on('data', function(data) {
+      a += data
+      console.log('Chunk received. Length: ', data.length);
+    });
+
+    response.on('end', function() {
+    console.log('Full stream data at a: ', a);
+    console.log('Response stream complete');
+
+    });
+
+  });
+}
+
+
+
+
+
+
+// getHTML (requestOptions, printHTML)
+// module.exports = function getHTML (options, callback) {
+//     /* Your code here */
+//     getHTML: getHTML
+// };
---
+++ requireTest.js
@@ -0,0 +1,15 @@
+
+var getHTML = require('./http-functions');
+
+
+function printHTML (html) {
+  console.log(html);
+};
+
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step5.html'
+};
+
+getHTML (requestOptions, printHTML);
