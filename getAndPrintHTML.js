46ad54c816241321e77bbcc10a63410c6e1a9f70
tranht86 <tran_ht@hotmail.com>
2017-04-10 17:11:40 +0000

    stored chunks into variable a, and output successfully

--- getAndPrintHTML.js
+++ getAndPrintHTML.js
@@ -1,10 +1,30 @@
-function getAndPrintHTML () {
+function getAndPrintHTML() {
 
   var requestOptions = {
     host: 'sytantris.github.io',
-    path: '/http-examples/step2.html'
+    path: '/html-examples/step2.html'
   };
 
-  /* Add your code here */
+  /* Add your code here*/
 
+  var https = require('https');
+  var a = ""
+
+  https.get(requestOptions, function (response) {
+
+    response.setEncoding('utf8');
+
+      response.on('data', function (data) {
+        a += data;
+      console.log('Chunk received. Length: ', data.length);
+      console.log(data)
+      });
+
+      response.on('end', function() {
+        console.log('Full stream data a: ', a);
+        console.log('Response stream complete.');
+      })
+  });
 }
+
+getAndPrintHTML()
